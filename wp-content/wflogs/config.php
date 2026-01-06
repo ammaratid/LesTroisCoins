8bf3cb52edb4919239d356d531e35c261988e198
Ammar Atid <atidammar19@gmail.com>
2026-01-06 23:38:29 +0100

    Nettoyage fichiers inutiles et sécurisation

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"*5luz`!FuLGTXJ_6=`-Q^I^kO{X:I~U47&5sKZVdJp5%(~?8$jjN~0J 64m$0E#y";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2066;}
